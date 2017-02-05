056098f0ce61b0b6a4d3e9b844a21face0544f7f
Kashif Nazir <knazir@stanford.edu>
2017-02-05 14:16:32 -0800

    Now to fix the socket

--- public/javascripts/client/loginController.js
+++ public/javascripts/client/loginController.js
@@ -1,4 +1,9 @@
 /* Login Controller */
 angular.module('main').controller('loginController', ['$scope', 'socket', function($scope, socket) {
     $scope.version = 'Beta';
+    $scope.message = '';
+
+    socket.on('time', function(timeString) {
+        $scope.message = 'Server time: ' + timeString;
+    });
 }]);
--- public/javascripts/client/mainController.js
+++ public/javascripts/client/mainController.js
@@ -1,9 +1,4 @@
 /* Main Controller */
 angular.module('main').controller('mainController', ['$scope', 'socket', function($scope, socket) {
-    $scope.version = 'Beta';
-    $scope.message = '';
 
-    socket.on('time', function(timeString) {
-        $scope.message = 'Server time: ' + timeString;
-    });
 }]);
